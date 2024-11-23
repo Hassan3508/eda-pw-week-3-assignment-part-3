5fe2b0b374b4b2a2f62db7afe9d6af6baf0329d3
ismail hassan <ismailshariff350@gmail.com>
2024-11-22 22:01:25 -0600

    variable called supply changes created and value it

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -11,6 +11,8 @@ console.log('Create partsNeeded:',partsNeeded);
 // 2. Create a variable called 'supplyChanges' whose value is an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11.
 console.log('2. Create supplyChanges:');
+let supplyChanges = [3, 5, -6, 0, 7, 11];
+console.log('Create supplyChanges:', supplyChanges);
 
 
 // 3. Create a variable called 'secondItem' and assign it the value of the second
